474a1350c78dc1bf7d09fcdbb7875f77cb09c7cb
Binu Peter <binupeterp07@gmail.com>
2023-02-01 10:52:08 +0000

    Week 2 Lab Done

---
+++ tasky/src/components/Form.js
@@ -0,0 +1,79 @@
+import React from "react";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+
+const AddTaskForm = (props) => {
+
+  return (
+    <Box
+    component="form"
+    sx={{
+      '& .MuiOutlinedInput-root': { m: 1, width: '30ch' },
+    }}
+    onSubmit={props.submit}
+  >
+
+<Box
+      component="form"
+      sx={{
+        '& .MuiOutlinedInput-root': { m: 1, width: '30ch' },
+      }}
+      onSubmit={props.submit}
+    >
+      <div>
+        <TextField
+          required
+          id="outlined-required"
+          name="title"
+          label="Task Title"
+          InputLabelProps={{ shrink: true }}
+          onChange={(event) => props.change(event)}
+        />
+      </div>
+      <div>
+        <TextField
+          required
+          name="deadline"
+          label="Deadline"
+          InputLabelProps={{ shrink: true }}
+          type="date"
+          onChange={(event) => props.change(event)}
+        />
+      </div>
+      <div>
+        <TextField
+          name="description"
+          id="outlined-multiline-static"
+          label="Task Details"
+          InputLabelProps={{ shrink: true }}
+          multiline
+          rows={4}
+          onChange={(event) => props.change(event)}
+        />
+      </div>
+      <div>
+        <Button 
+          type="submit"
+          variant="contained"
+          color="primary"
+          sx={{
+            m: 1,
+            p: 1,
+            width: '95%'
+          }}
+        >
+          Add Task
+        </Button>
+      </div>
+    </Box>
+
+
+
+  </Box>
+
+  )
+};
+
+export default AddTaskForm;
